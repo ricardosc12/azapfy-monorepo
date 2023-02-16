321e3da2a3fd35a83865c28e3c93ac0ebbabbbaa
Ricardo <ricardo.spinola9@gmail.com>
2023-02-16 17:44:51 -0300

    fix

--- src/library/ui/src/atoms/Icons/index.tsx
+++ src/library/ui/src/atoms/Icons/index.tsx
@@ -1,10 +1,10 @@
 import styled from "@emotion/styled"
 import { IconButton } from "@mui/material"
 
-const IconWrapper = styled(IconButton)`
+const IconWrapper:any = styled(IconButton)`
     & > svg {
         font-size: 20px;
-        color: ${props=>props.iconColor?props.iconColor:'var(--text_comum)'};
+        color: ${props=>'var(--text_comum)'};
     }
 `
 
