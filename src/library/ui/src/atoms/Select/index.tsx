7defb82229f99166b1a3bf54c85940c6a574a7fb
Ricardo <ricardo.spinola9@gmail.com>
2023-03-03 13:00:45 -0300

    teste drop

--- src/library/ui/src/atoms/Select/index.tsx
+++ src/library/ui/src/atoms/Select/index.tsx
@@ -1,32 +1,82 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import WindowedSelect from "react-windowed-select";
 import Dropdown from "./component";
+import styled from "@emotion/styled";
+import TextField from "../../atoms/TextField";
 
-const WSelect:any = WindowedSelect
+const Main = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	grid-gap: 2rem;
+	& #input_area {
+		input {
+			width: 8rem;
+		}
+	}
+`;
 
-const options:any = [];
+const WSelect: any = WindowedSelect;
 
-for (let i = 0; i < 100000; i += 1) {
-  options.push({
-    label: `teste ${i}`,
-    value: i
-  });
-}
+// const options: any = [];
 
-const Select_=()=>{
-    const [value, setValue] = useState()
+// for (let i = 0; i < 100000; i += 1) {
+//     options.push({
+//         label: `teste ${i}`,
+//         value: i,
+//     });
+// }
 
-    return <Dropdown input={{value:value,onChange:(e:any)=>setValue(e)}} data={options} label="Options" meta={{}}/>
-
-}
-
-export default function Select(){
+const Select_ = ({data}:any) => {
+    const [value, setValue] = useState();
 
     return (
-        <div>
-            <Select_/>
-            <WSelect options={options} />
-        </div>
+        <Dropdown
+            input={{ value: value, onChange: (e: any) => setValue(e) }}
+            data={data}
+            label="Options"
+            meta={{}}
+        />
+    );
+};
 
-    )
+export default function Select() {
+
+    const [data, setData] = useState([])
+    const refDefault = useRef(1000)
+
+    const refInput:any=useRef()
+
+    function getData(num:any){
+        const options: any = [];
+        for (let i = 0; i < num; i += 1) {
+            options.push({
+                label: `teste ${i}`,
+                value: i,
+            });
+        }
+        return options
+    }
+
+    useEffect(()=>{
+        setData(getData(refDefault.current))
+    },[])
+
+    function getValue(){
+        setData(getData(refInput.current.value))
+    }
+
+    return (
+        <Main>
+            <h1>Data: {data.length}</h1>
+            <div>
+                <Select_ data={data}/>
+                <WSelect options={data} />
+            </div>
+            <div id="input_area">
+                <TextField inputRef={refInput} id="data" defaultValue={refDefault.current}/>
+                <button onClick={getValue}>set_data</button>
+            </div>
+        </Main>
+    );
 }
--- src/library/ui/src/atoms/TextField/index.tsx
+++ src/library/ui/src/atoms/TextField/index.tsx
@@ -1,3 +1,7 @@
-export default function TextField() {
-    return <input type="text" />;
+import { TextField as TextFieldMui, TextFieldProps } from "@mui/material";
+
+
+
+export default function TextField(props:TextFieldProps) {
+    return <TextFieldMui  {...props}/>;
 }
